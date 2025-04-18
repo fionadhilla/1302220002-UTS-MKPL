51ef9ad83141daa97e552e48f6b5f61a38d3657f
fionadhilla <fiona08dhilla@gmail.com>
2025-04-19 05:07:31 +0700

    perubahan sebelum merge BugFix-Employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -25,22 +25,6 @@ public class Employee {
 		this.dayJoined = dayJoined;
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	
-	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
-	
-	
 	
 	public int getAnnualIncomeTax() {
 		
